a3abf327ad50e859740bc907b933ebee3eb824d0
Mathieu Scheltienne <mathieu.scheltienne@fcbg.ch>
2023-07-06 15:34:57 +0200

    rm progress bars

--- macos/setup.sh
+++ macos/setup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash -e
 
 python -m pip install --progress-bar off --upgrade pip setuptools wheel
-python -m pip install psychopy psychtoolbox
+python -m pip install --progress-bar off psychopy psychtoolbox
