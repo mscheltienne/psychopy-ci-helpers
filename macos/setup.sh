edfbe0b88999b794d0aeddd6593dc2e594749d4c
Mathieu Scheltienne <mathieu.scheltienne@fcbg.ch>
2023-07-06 16:00:19 +0200

    try with homebrew

--- macos/setup.sh
+++ macos/setup.sh
@@ -3,6 +3,6 @@
 python -m pip install --progress-bar off --upgrade pip setuptools wheel
 readonly py_version=$(python -c 'import sys; print("".join(map(str, sys.version_info[:2])))')
 if [[ $py_version != "39" ]]; then
-    python -m pip install attrdict3
+    brew install wxpython
 fi
 python -m pip install --progress-bar off psychopy psychtoolbox
