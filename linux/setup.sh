28ba840bd25d6824af127423d1376fb6e2779e57
Mathieu Scheltienne <mathieu.scheltienne@fcbg.ch>
2023-07-06 14:38:18 +0200

    add simple linux version

---
+++ linux/setup.sh
@@ -0,0 +1,9 @@
+#!/bin/bash -e
+
+sudo apt install libusb-1.0-0-dev portaudio19-dev libasound2-dev libsdl2-2.0-0
+python -m pip install --progress-bar off --upgrade pip setuptools wheel
+readonly py_version="$(python -c 'import sys; print("".join(map(str, sys.version_info[:2])))')"
+readonly wx_name="wxPython-4.2.1-cp${PYV}-cp${PYV}-linux_x86_64.whl"
+curl https://extras.wxpython.org/wxPython4/extras/linux/gtk3/ubuntu-22.04/${wx_name} -o ${wx_name}
+python -m pip install ${wx_name}
+python -m pip install psychopy psychtoolbox
