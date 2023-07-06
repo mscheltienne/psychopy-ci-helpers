f65aef09169bb6caa01ad5ab3b7a27806db5f002
Mathieu Scheltienne <mathieu.scheltienne@fcbg.ch>
2023-07-06 16:21:35 +0200

    restrict to supported psychopy versions

--- macos/setup.sh
+++ macos/setup.sh
@@ -2,9 +2,9 @@
 
 python -m pip install --progress-bar off --upgrade pip setuptools wheel
 readonly py_version=$(python -c 'import sys; print("".join(map(str, sys.version_info[:2])))')
-if [[ $py_version != "39" ]]; then
+if [[ $py_version == "310" ]]; then
     python -m pip install --progress-bar off git+https://github.com/psychopy/psychopy
 else
-    python -m pip install --progress-bar off
+    python -m pip install --progress-bar off psychopy
 fi
 python -m pip install --progress-bar off psychtoolbox
