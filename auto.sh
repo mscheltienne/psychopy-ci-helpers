caadb8e2730fa71f89e9c2e8e9fd2382d7b861e2
Mathieu Scheltienne <mathieu.scheltienne@fcbg.ch>
2023-07-06 15:03:31 +0200

    add action and try bash on windows

--- auto.sh
+++ auto.sh
@@ -4,5 +4,5 @@ THIS_DIR=$(dirname "$0")
 case "$OSTYPE" in
 	darwin*)  echo "macOS ($OSTYPE)" && $THIS_DIR/macos/setup.sh ;;
 	linux*)   echo "Linux ($OSTYPE)" && $THIS_DIR/linux/setup.sh ;;
-	*)        echo "Windows ($OSTYPE)" && powershell $THIS_DIR/windows/setup.ps1 ;;
+	*)        echo "Windows ($OSTYPE)" && $THIS_DIR/windows/setup.sh ;;
 esac
