aab995d4d1cd67ea337b92e8c81575a0171ace9e
Mathieu Scheltienne <mathieu.scheltienne@fcbg.ch>
2023-07-06 16:14:14 +0200

    try dev version

--- macos/setup.sh
+++ macos/setup.sh
@@ -3,6 +3,8 @@
 python -m pip install --progress-bar off --upgrade pip setuptools wheel
 readonly py_version=$(python -c 'import sys; print("".join(map(str, sys.version_info[:2])))')
 if [[ $py_version != "39" ]]; then
-    brew install wxpython
+    python -m pip install --progress-bar off git+https://github.com/psychopy/psychopy
+else
+    python -m pip install --progress-bar off
 fi
-python -m pip install --progress-bar off psychopy psychtoolbox
+python -m pip install --progress-bar off psychtoolbox
