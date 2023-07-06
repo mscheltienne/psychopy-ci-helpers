e7a124dc745df81fd3b677c58e90a7a8cfb8df0e
Mathieu Scheltienne <mathieu.scheltienne@fcbg.ch>
2023-07-06 15:42:39 +0200

    fix typo

--- macos/setup.sh
+++ macos/setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -e
 
-brew updade
+brew update
 brew install wxwidgets
 python -m pip install --progress-bar off --upgrade pip setuptools wheel
 python -m pip install --progress-bar off psychopy psychtoolbox
