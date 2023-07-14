333bf20c4805d43f89485aff7f417c5d4b04afc1
Mathieu Scheltienne <mathieu.scheltienne@fcbg.ch>
2023-07-14 09:58:21 +0200

    rm progressbar for linux install

--- linux/setup.sh
+++ linux/setup.sh
@@ -5,5 +5,5 @@ python -m pip install --progress-bar off --upgrade pip setuptools wheel
 readonly py_version=$(python -c 'import sys; print("".join(map(str, sys.version_info[:2])))')
 readonly wx_name="wxPython-4.2.1-cp${py_version}-cp${py_version}-linux_x86_64.whl"
 curl https://extras.wxpython.org/wxPython4/extras/linux/gtk3/ubuntu-22.04/${wx_name} -o ${wx_name}
-python -m pip install ${wx_name}
-python -m pip install psychopy psychtoolbox
+python -m pip install --progress-bar off ${wx_name}
+python -m pip install --progress-bar off psychopy psychtoolbox
